5115ddf04e066223bd5304516ee9d9ecad02992f
Apoorv-Vardhman <apoorv.vardhman@gmail.com>
2022-03-14 19:49:50 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	// define a variable 
+	$name = "Apoorv";
+	$last  = "Vardhman";
+	echo "My name is ".$name." ".$last;
+	echo "<br>";
+	echo 'My name is '.$name;
+	echo "<br>";
+	echo "My name is $name $last";
+	
+?>
